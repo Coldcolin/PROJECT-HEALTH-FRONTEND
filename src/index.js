d14491b6dcd4ecfaabc4f76a322061bc993ced65
Coldcolin <decorcecolin@gmail.com>
2022-07-04 15:52:03 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from "./Components/Global/AuthProvider.js"
+import { Provider } from "react-redux"
+import { store, Persistor } from "./Components/Global/store"
+import { PersistGate } from 'redux-persist/integration/react'
+// import { store } from "./Components/Global/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+  <AuthProvider>
+  <PersistGate loading={null} persistor={Persistor}>
+  <App />
+  </PersistGate>
+  </AuthProvider>
+  </Provider>
   </React.StrictMode>
 );
 
